4d1100f0a69dc9d1ac9ec7bb21b9d3fd2143ba22
ansorokey <ansorokey@gmail.com>
2023-12-13 13:10:08 -0500

    completed comments and readme

--- README.md
+++ README.md
@@ -4,7 +4,14 @@ Interact with a page's background and modify visibility
 This project was created by following [How To Make Image Background Change Effect On Website Using HTML CSS And JavaScript](https://youtu.be/Z7kmaWcMJBY?si=4lcV7Jv_SfPwzbZ5)
 
 ## Key Features and Technologies
+- JavaScript, HTML, CSS
+- DOM Manipulation
+- Event Listeners
 
 ## How To Use
+Move your cursor over the image to shift the appearance of a transparent background from left to right.
 
 ## Future Features
+- Make the cursor bar/elemnent on the image click and drag, rather than follow the mouse
+- Additionally, have the cursor stay in place when released from drag
+- Apply the same functionality, but wipe vertically instead
--- script.js
+++ script.js
@@ -11,10 +11,21 @@ let leftSpace = imgBox.offsetLeft;
 // This is the width of the current element, margin excluded
 original.style.width = imgBox.offsetWidth + "px";
 
+// Behind the scenes, we are layering two similar images
+// When we move the mouse, we're changing the size of one image
+// container over the another
+
 imgBox.onmousemove = function(e) {
     // e.pageX tracks the x coordinate of the mouse on the page
     // specifically, while over the img-box element
+
+    // Every time the mouse moves over the img-box,
+    // the width of the element is set to that coordinate,
+    // minus the empty space to account for offset
     let boxWidth = (e.pageX - leftSpace) + "px";
     imgWrap.style.width = boxWidth;
+
+    // the cursor's absolute left position is also
+    // set to the same width to look like a cursor
     line.style.left = boxWidth;
 }
